f60dcb03355f8b03cecebd07a2a1490fd12d448f
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-02-05 17:27:10 +0100

    fix errors

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const WebSocket = require("ws");
 
 const wsServer = new WebSocket.Server({ port: 8080 });
 
-wsServer.on('connection', connection => {
-  console.log('somebody connected')
-  connection.send('Hi there!')
-  connection.on('message', message => {
-    wsServer.clients.forEach(function each (client){
-      client.send(message)
-    })
-
-  })
-})
+wsServer.on("connection", (connection) => {
+  console.log("somebody connected");
+  connection.send("Hi there!");
+  connection.on("message", (message) => {
+    message = JSON.parse(message);
+    wsServer.clients.forEach(function each(client) {
+      client.send(JSON.stringify(message));
+    });
+  });
+});
